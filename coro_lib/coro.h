fbecebf732c0937e3683d669ddd290227dd52a2b
nocmok <nyusafonov@edu.hse.ru>
2020-07-11 11:54:01 +0300

    struct yield_buf removed from coro.h file

--- coro_lib/coro.h
+++ coro_lib/coro.h
@@ -6,14 +6,8 @@
 #define CORO_DEFAULT_STACK_SIZE 4096
 
 typedef struct coro_t coro_t;
-typedef struct yield_buf yield_buf;
 
-typedef void (*coro_fn)(yield_buf*);
-
-struct yield_buf{
-    ucontext_t *caller_context;
-    void *data;
-};
+typedef void (*coro_fn)();
 
 struct coro_t {
     
@@ -23,11 +17,15 @@ struct coro_t {
     coro_fn func;
 
     ucontext_t this_context;
+    ucontext_t caller_context;
 };
 
 coro_t *coro_curr();
 void coro_init(coro_t *coro, coro_fn func);
-void coro_yield(coro_t *next, void *data);
-void coro_start(coro_t *coro, void *data);
+
+/* transfers control to execution context of specified coroutine, or to caller execution context if next == NULL */
+void coro_yield(coro_t *next);
+void coro_start(coro_t *coro);
+void coro_free(coro_t *coro); 
 
 #endif
