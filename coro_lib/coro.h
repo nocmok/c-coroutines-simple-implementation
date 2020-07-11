2cc78e479718a8997a817514cbf40bb22c151abd
nocmok <nyusafonov@edu.hse.ru>
2020-07-11 11:17:54 +0300

    header file structure created

--- coro_lib/coro.h
+++ coro_lib/coro.h
@@ -1,4 +1,31 @@
 #ifndef CORO_H
 #define CORO_H
 
+#include <ucontext.h>
+
+typedef struct coro_t coro_t;
+typedef struct yield_buf yield_buf;
+
+typedef void (*coro_fn)(yield_buf*);
+
+struct yield_buf{
+    ucontext_t *caller_context;
+    void *data;
+};
+
+struct coro_t {
+    
+    size_t stack_size;
+    void *stack;
+
+    coro_fn func;
+
+    ucontext_t this_context;
+};
+
+coro_t* coro_curr();
+void coro_init(coro_t *coro, coro_fn func);
+void coro_yield(coro_t *next, void *data);
+void coro_start(coro_t *coro);
+
 #endif
