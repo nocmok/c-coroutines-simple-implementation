dab8d13f325435ccd08cd4a5c96b81be1f739487
nocmok <nyusafonov@edu.hse.ru>
2020-07-11 13:38:23 +0300

    coro.h file coro_state added

--- coro_lib/coro.h
+++ coro_lib/coro.h
@@ -6,24 +6,34 @@
 #define CORO_DEFAULT_STACK_SIZE 4096
 
 typedef struct coro_t coro_t;
+typedef enum coro_state coro_state;
 
-typedef void (*coro_fn)();
+typedef void (*coro_fn)(coro_t *coro);
+
+enum coro_state {
+    NEW,
+    RUNNING,
+    FINISHED,
+    FREED
+};
 
 struct coro_t {
-    
+    coro_state state;
+
     size_t stack_size;
     void *stack;
 
     coro_fn func;
 
     ucontext_t this_context;
-    ucontext_t caller_context;
+    /* helps to transfer control to execution context of caller, if coroutine done all it's work.
+    * Since other coroutine may appear as the caller, so the context may changes dynamically, 
+    * this field holds pointer to context, not context itself.
+     */
+    ucontext_t *caller_context;
 };
 
-coro_t *coro_curr();
 void coro_init(coro_t *coro, coro_fn func);
-
-/* transfers control to execution context of specified coroutine, or to caller execution context if next == NULL */
 void coro_yield(coro_t *next);
 void coro_start(coro_t *coro);
 void coro_free(coro_t *coro); 
